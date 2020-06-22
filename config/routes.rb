74151818adc4834b4342a1952b1e88db6ea1eae5
dancreppel <creppel@usc.edu>
2020-06-22 15:41:30 -0700

    Update user route to get users

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#root'
 
   namespace :api, defaults: { format: json } do
-    resources :users, only: [:create, :show]
+    resources :users, only: [:create, :show, :index]
     resoruce :session, only: [:create, :delete]
   end
 end

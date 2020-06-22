d2cc538478110cb0fcc0b57060b5bef4a98f5532
dancreppel <creppel@usc.edu>
2020-06-22 16:41:45 -0700

    Define create action for session controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,2 +1,10 @@
 class Api::SessionsController < ApplicationController
+  def create
+    @user = User.find_by_credentials(user_params.username, user_params.password)
+    if @user
+      render :show
+    else
+      render json: ['Invalid Credentials'], status: 401
+    end
+  end
 end

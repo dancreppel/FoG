3ffc995a01eb9e748c968b0768e6f134bd433a8d
dancreppel <creppel@usc.edu>
2020-06-22 16:46:37 -0700

    Define destroy action for session controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,10 +1,18 @@
 class Api::SessionsController < ApplicationController
+  before_action :require_login, only: :destroy
+
   def create
     @user = User.find_by_credentials(user_params.username, user_params.password)
     if @user
+      login!(@user)
       render :show
     else
       render json: ['Invalid Credentials'], status: 401
     end
+  end
+
+  def destroy
+    logout!
+    render json: {}
   end
 end

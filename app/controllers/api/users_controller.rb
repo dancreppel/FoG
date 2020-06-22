b2299e37986612066b784acf36574bc6ef77adb8
dancreppel <creppel@usc.edu>
2020-06-22 15:47:09 -0700

    Define the create action for users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,17 @@
 class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 422
+    end
+  end
+  
+  def index
+    # do not need to implement
+  end
+
+  def show
+  end
 end

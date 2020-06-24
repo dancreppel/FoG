4b4875ee00c34ceffaa33d6c1f8b1f329ad0886b
dancreppel <creppel@usc.edu>
2020-06-24 13:59:41 -0700

    Create auth route

---
+++ frontend/components/routes/auth_route.jsx
@@ -0,0 +1,24 @@
+import { Route, Redirect } from 'react-router-dom';
+import React from 'react';
+import { connect } from 'react-redux';
+
+const Auth = ({ path, component: Component, loggedIn, exact }) => (
+  <Route 
+    path={path}
+    exact={exact}
+    render={(props) => (
+      loggedIn ? (
+        <Redirect to='/' />
+      ) : (
+        <Component {...props} />
+      )
+    )}
+  />
+);
+
+const MSP = ({ sessionUser }) = ({
+  loggedIn: Boolean(sessionUser)
+})
+
+const AuthRoute = connect(MSP, null)(Auth);
+export default AuthRoute;

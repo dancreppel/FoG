9b9a0759021b43a5d74cf375e2a8f2e696a11ccd
dancreppel <creppel@usc.edu>
2020-06-24 09:29:58 -0700

    Add login and sign up routes in app

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './header/header';
+import LoginContainer from './session_forms/login_container';
+import SignupContainer from './session_forms/signup_container';
 
 const App = () => (
   <>
     <Header />
 
-    {/* <Route path='/login'/>
-    <Route path='/signup'/> */}
+    <Route path='/login' component={LoginContainer} />
+    <Route path='/signup'component={SignupContainer} />
   </>
 );
 

92f38e7bbc5c48b025d7d676449efc4d3b2d5635
dancreppel <creppel@usc.edu>
2020-06-24 11:16:39 -0700

    update session containers to allow clearing of errors

--- frontend/components/session_forms/login_container.js
+++ frontend/components/session_forms/login_container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import LoginForm from './login_form';
 import { login } from '../../actions/session_actions';
+import { clearErrors } from '../../actions/errors_actions';
 
 const MSP = ({ errors }) => ({
   formType: 'Sign In',
@@ -8,7 +9,8 @@ const MSP = ({ errors }) => ({
 });
 
 const MDP = dispatch => ({
-  action: user => dispatch(login(user))
+  action: user => dispatch(login(user)),
+  clearErrors: () => dispatch(clearErrors())
 });
 
 const LoginContainer = connect(MSP, MDP)(LoginForm);
--- frontend/components/session_forms/signup_container.js
+++ frontend/components/session_forms/signup_container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux';
 import SessionForm from './session_form';
 import { signup } from '../../actions/session_actions';
+import { clearErrors } from '../../actions/errors_actions';
 
 const MSP = ({ errors }) => ({
   formType: 'Join FoG',
@@ -8,7 +9,8 @@ const MSP = ({ errors }) => ({
 });
 
 const MDP = dispatch => ({
-  action: user => dispatch(signup(user))
+  action: user => dispatch(signup(user)),
+  clearErrors: () => dispatch(clearErrors())
 });
 
 const SignupContainer = connect(MSP, MDP)(SessionForm);

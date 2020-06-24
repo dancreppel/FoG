c3c6d21da9cee77fa79889582cfef4dc80fc7523
dancreppel <creppel@usc.edu>
2020-06-24 15:06:17 -0700

    add link to material icons in application headand add icon to header user

--- frontend/components/header/header_user.jsx
+++ frontend/components/header/header_user.jsx
@@ -3,10 +3,13 @@ import { Link } from 'react-router-dom'
 
 const HeaderUser = ({ currentUser, logout }) => (
   currentUser ? (
-    <>
+    <div>
       <h1 className='headerUser'>{currentUser.username}</h1>
-      <button className='headerLogout' onClick={logout}>logout</button>
-    </>
+      <span className="material-icons">
+        arrow_drop_down
+        <button className='headerLogout hidden' onClick={logout}>logout</button>
+      </span>
+    </div>
   ) : (
     <Link to='/login'>login</Link>
   )

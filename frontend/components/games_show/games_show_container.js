757d468ad5f8b8a221af58857e05d1924ed71692
dancreppel <creppel@usc.edu>
2020-06-30 11:22:48 -0700

    fix props

--- frontend/components/games_show/games_show_container.js
+++ frontend/components/games_show/games_show_container.js
@@ -1,15 +1,22 @@
 import { connect } from 'react-redux';
 import GamesShow from './games_show';
 import { fetchGame } from '../../actions/games_actions';
-import genresForGame from '../../selectors/games_genres';
+import { genresForGame } from '../../selectors/games_genres';
+import { fetchGenres } from '../../actions/genres_actions';
+import { fetchGamesGenres } from '../../actions/games_genres_actions';
 
-const MSP = ({ entities: { games, genres, gamesGenres } }, ownProps ) => ({
-  game: games[ownProps.match.params.id],
-  genres: genresForGame(genres, gamesGenres, ownProps.match.params.id)
-})
+
+const MSP = ({ entities: { games, genres, gamesGenres } }, ownProps ) => {
+  return {
+    game: games[ownProps.match.params.id],
+    genres: genresForGame(genres, gamesGenres, ownProps.match.params.id)
+  }
+}
 
 const MDP = dispatch => ({
-  fetchGame: gameId => dispatch(fetchGame(gameId))
+  fetchGame: gameId => dispatch(fetchGame(gameId)),
+  fetchGenres: () => dispatch(fetchGenres()),
+  fetchGamesGenres: () => dispatch(fetchGamesGenres())
 });
 
 export default connect(MSP, MDP)(GamesShow);

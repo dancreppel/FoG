c2588b59774c06553864b5de5d2bd49e3799df15
dancreppel <creppel@usc.edu>
2020-06-29 20:27:53 -0700

    add route for game show page

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import Header from './header/header';
 import SessionRoutes from './session/session_routes';
 import GamesIndex from './games_index/games_index';
+import GameShowContainer from './games_show/games_show_container';
 
 // Todo test slider
 import Slider from './slider/slider';
@@ -12,6 +13,7 @@ const App = () => (
     <Header />
     <SessionRoutes />
     {/* <Route exact path='/' component={GamesIndex} /> */}
+    <Route path='/games/:id' component={GameShowContainer} />
     <Slider />
   </>
 );

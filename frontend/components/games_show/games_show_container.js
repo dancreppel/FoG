9f40ab80a988e2bf03354d2fe9ecffc32a37604a
dancreppel <creppel@usc.edu>
2020-06-29 20:29:21 -0700

    connect games show page

---
+++ frontend/components/games_show/games_show_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import GamesShow from './games_show';
+import { fetchGame } from '../../actions/games_actions';
+
+const MSP = ({ state: { games } }, ownProps ) => ({
+  game: games[ownProps.match.params.id]
+})
+
+const MDP = dispatch => ({
+  fetchGame: gameId => dispatch(fetchGame(gameId))
+});
+
+export default connect(MSP, MDP)(GamesShow);

8b31462c7c5292e845e06b9bd68ac9c1c0fc9907
dancreppel <creppel@usc.edu>
2020-06-30 11:22:13 -0700

    correctly filter genres

--- frontend/selectors/games_genres.js
+++ frontend/selectors/games_genres.js
@@ -1,12 +1,19 @@
 // search for all genres that belong to a game
 export const genresForGame = (genres, gamesGenres, gameId) => {
-  let i;
-  let genresArr = [];
-  for(i of gamesGenres) {
-    if (i.gameId === gameId) {
-      let genre = genres[i.genreId];
-      genresArr.push(genre.name)
+  // prevent logic if genres and gamesGenres have not been fetched yet
+  if (!genres[1] || !gamesGenres[1]) return null;
+
+  let genresIds = [];
+  for(let i = 1; i <= Object.keys(gamesGenres).length; i++) {
+    if (gamesGenres[i].gameId === Number(gameId)) {
+      genresIds.push(gamesGenres[i].genreId)
     }
   }
+
+  let genresArr = [];
+  for(let id of genresIds) {
+    genresArr.push(genres[id].name);
+  }
+
   return genresArr;
 };

3cb62004f73d0268a9301f21105b769526321b7f
dancreppel <creppel@usc.edu>
2020-06-30 09:24:57 -0700

    modify map states to props to take in genres

--- frontend/components/games_show/games_show_container.js
+++ frontend/components/games_show/games_show_container.js
@@ -1,9 +1,11 @@
 import { connect } from 'react-redux';
 import GamesShow from './games_show';
 import { fetchGame } from '../../actions/games_actions';
+import genresForGame from '../../selectors/games_genres';
 
-const MSP = ({ entities: { games } }, ownProps ) => ({
-  game: games[ownProps.match.params.id]
+const MSP = ({ entities: { games, genres, gamesGenres } }, ownProps ) => ({
+  game: games[ownProps.match.params.id],
+  genres: genresForGame(genres, gamesGenres, ownProps.match.params.id)
 })
 
 const MDP = dispatch => ({

63de872e9ac78cc61a53bb9521eaa017188d5b9b
dancreppel <creppel@usc.edu>
2020-06-29 23:36:41 -0700

    create games genres actions

---
+++ frontend/actions/games_genres_actions.js
@@ -0,0 +1,13 @@
+import getGamesGenres from "../util/games_genres_api_util";
+
+export const RECEIVE_GAMES_GENRES = "RECEIVE_GENRES";
+
+export const receiveGamesGenres = (gamesGenres) => ({
+  type: RECEIVE_GAMES_GENRES,
+  gamesGenres,
+});
+
+export const fetchGamesGenres = () => (dispatch) => (
+  getGamesGenres()
+    .then((gamesGenres) => dispatch(receiveGamesGenres(gamesGenres)))
+);

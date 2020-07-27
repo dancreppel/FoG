f70c99593213802e97a0f9427420b405a1842f75
dancreppel <creppel@usc.edu>
2020-07-27 16:57:50 -0700

    Use lodash.shuffle to randomize games

--- frontend/components/games_index/games_index_container.js
+++ frontend/components/games_index/games_index_container.js
@@ -1,9 +1,11 @@
 import { connect } from 'react-redux';
 import GamesIndex from './games_index';
 import { fetchGames } from '../../actions/games_actions';
+import lodash from 'lodash';
 
+// Randomize order of games
 const MSP =({ entities: { games }}) => ({
-  games: Object.values(games)
+  games: lodash.shuffle(Object.values(games))
 });
 
 const MDP = dispatch => ({

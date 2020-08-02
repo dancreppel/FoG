0fd1b353ccca2fde892ab279ae3420bfbe69ab9c
dancreppel <creppel@usc.edu>
2020-08-02 15:05:48 -0700

    Update reducer to update discounts

--- frontend/reducers/entities/games_reducer.js
+++ frontend/reducers/entities/games_reducer.js
@@ -1,6 +1,7 @@
 import {
   RECEIVE_GAMES,
-  RECEIVE_GAME
+  RECEIVE_GAME,
+  RECIEVE_DISCOUNT
 } from '../../actions/games_actions';
 
 const gamesReducer = (state = {}, action) => {
@@ -12,6 +13,9 @@ const gamesReducer = (state = {}, action) => {
       return action.games;
     case RECEIVE_GAME:
       newState[action.game.id] = action.game;
+      return newState;
+    case RECIEVE_DISCOUNT:
+      newState[action.gameId].discount = action.discount;
       return newState;
     default:
       return state;

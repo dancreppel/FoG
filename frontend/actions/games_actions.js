033dbc8424f2beca8efca7f8b187a9ba3233b9b9
dancreppel <creppel@usc.edu>
2020-08-02 15:04:38 -0700

    Add thunk action creator to update game's discount

--- frontend/actions/games_actions.js
+++ frontend/actions/games_actions.js
@@ -4,6 +4,7 @@ import lodash from "lodash";
 
 export const RECEIVE_GAMES = 'RECEIVE_GAMES';
 export const RECEIVE_GAME = 'RECEIVE_GAME';
+export const RECIEVE_DISCOUNT = 'RECEIVE_DISCOUNT';
 
 export const receiveGames = games => ({
   type: RECEIVE_GAMES,
@@ -13,6 +14,12 @@ export const receiveGames = games => ({
 export const receiveGame = game => ({
   type: RECEIVE_GAME,
   game
+});
+
+export const receiveDiscount = (gameId, discount) => ({
+  type: RECIEVE_DISCOUNT,
+  gameId,
+  discount
 });
 
 // Shuffle the order of games
@@ -28,6 +35,14 @@ export const fetchGame = gameId => dispatch => (
   GameAPIUtil.getGame(gameId)
     .then(
       game => dispatch(receiveGame(game)),
+      errors => dispatch(receiveErrors(errors.responseJSON))
+    )
+);
+
+export const updateDiscount = (gameId, discount) => dispatch => (
+  GameAPIUtil.patchDiscount
+    .then(
+      () => dispatch(receiveDiscount(gameId, discount)),
       errors => dispatch(receiveErrors(errors.responseJSON))
     )
 );

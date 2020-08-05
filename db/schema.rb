8cc1ccf217fd5369b09f38d2ba592fa54b6eb73c
dancreppel <creppel@usc.edu>
2020-08-04 17:28:01 -0700

    Add reviews table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_211722) do
+ActiveRecord::Schema.define(version: 2020_08_04_225332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,16 @@ ActiveRecord::Schema.define(version: 2020_08_02_211722) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_genres_on_name"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.integer "author_id", null: false
+    t.integer "game_id", null: false
+    t.boolean "recommended", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id", "game_id"], name: "index_reviews_on_author_id_and_game_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|

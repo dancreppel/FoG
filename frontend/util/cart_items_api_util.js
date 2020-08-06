7d36ec7f1b0b6eaa92dea5575060323e34cf87c4
dancreppel <creppel@usc.edu>
2020-08-06 16:20:31 -0700

    Create ajax request for creating a cart item for current user

--- frontend/util/cart_items_api_util.js
+++ frontend/util/cart_items_api_util.js
@@ -2,4 +2,12 @@ export const getCartItems = () => (
   $.ajax({
     url: "api/cart_items"
   })
-)
+);
+
+export const postCartItem = gameId => (
+  $.ajax({
+    url: "api/cart_items",
+    method: "post",
+    data: { cart: { game_id: gameId }}
+  })
+);

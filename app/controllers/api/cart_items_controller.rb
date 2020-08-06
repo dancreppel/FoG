00863af35ede523f8429e143b0b3ad3e23c5bd28
dancreppel <creppel@usc.edu>
2020-08-05 17:41:29 -0700

    Add create action for cart items controller

--- app/controllers/api/cart_items_controller.rb
+++ app/controllers/api/cart_items_controller.rb
@@ -1,8 +1,8 @@
 class Api::CartItemsController < ApplicationController
-  :before_action :require_login
+  # :before_action :require_login
 
   def index
-    @cart_games = User.find_by(id: params.user_id).includes(:cart_games)
+    @cart_games = User.find_by(id: current_user.id).includes(:cart_games)
     debugger
     if @cart_games
       # render :index
@@ -10,5 +10,23 @@ class Api::CartItemsController < ApplicationController
     else
       render json: "No games found", status: 404
     end
+  end
+
+  def create
+    @cart_item = CartItem.new(
+      user_id: current_user.id, 
+      game_id: cart_params['game_id']
+    )
+
+    if @cart_item.save
+      @cart_game = @cart_item.game
+      render json: @cart_game
+    else
+      render json: @cart_item.errors.full_messages, status: 422
+    end
+  end
+
+  def cart_params 
+    params.require(:cart).permit(:game_id)
   end
 end

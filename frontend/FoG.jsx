7959e964559b136cb66e147962edd8082be39e23
dancreppel <creppel@usc.edu>
2020-08-12 13:57:43 -0700

    Add front page image to assets

--- frontend/FoG.jsx
+++ frontend/FoG.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
+import * as CartActions from "./actions/cart_actions"
 
 document.addEventListener( 'DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -10,6 +11,12 @@ document.addEventListener( 'DOMContentLoaded', () => {
   if (window.currentUser) preloadedState = { sessionUser: currentUser }
   const store = configureStore(preloadedState);
   delete window.currentUser;
+
+  // TODO TEST CART REDUCER WITH THIS
+  window.dispatch = store.dispatch;
+  window.fetchCartItems = CartActions.fetchCartItems;
+  window.createCartItem = CartActions.createCartItem;
+  window.deleteCartItem = CartActions.deleteCartItem;
 
   ReactDOM.render(<Root store={store}/>, root)
 });

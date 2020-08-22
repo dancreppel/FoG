0e1998256f0dbdd2a78a22db82887c8bb98f6e39
dancreppel <creppel@usc.edu>
2020-08-21 17:30:11 -0700

    Create cart component

---
+++ frontend/components/cart/cart.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import CartItem from './cart_item';
+
+export default class Cart extends React.Component {
+  componentDidMount () {
+    this.props.getUserCart();
+  }
+
+  render () {
+    let cart = Object.values(this.props.cart);
+    let games = this.props.games;
+
+    let cartItems = cart.map((cartItem, index) => (
+      <CartItem 
+        key={'cart item ' + index}
+        game={games[cartItem.gameId]} 
+        cartId={cartItem.id}
+        removeCartItem={this.props.removeCartItem}
+      />
+    ));
+
+    return (
+      <div className="cart">
+        <div className="cart-component">
+
+          <h1 className="page-title">Your Shopping Cart</h1>
+
+          <ul className="cart-items">
+            {cartItems}
+          </ul>
+
+          <div className="payment">
+            <div className="total-price">
+
+            </div>
+
+            <button className="purchase">Purchase</button>
+          </div>
+          
+        </div>
+
+      </div>
+    )
+  }
+}

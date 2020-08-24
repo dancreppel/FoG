d3c9723ae544af4082ab888509a8fc9d5f6a8467
dancreppel <creppel@usc.edu>
2020-08-24 15:26:32 -0700

    Cycle through images when item  details are not hidden

--- frontend/components/cart/cart_item.jsx
+++ frontend/components/cart/cart_item.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { genresForGame } from "../../selectors/games_genres";
-import lodash from 'lodash'
+import lodash, { set } from 'lodash'
 
 export default class CartItem extends React.Component {
   constructor (props) {
     super(props);
-    // this.state = { hidden: true };
+    this.state = { 
+      index: 0,
+      hidden: true
+    };
   }
 
-  componentDidMount () {
-    this.setState({ hidden: true });
+  // componentDidMount () {
+  //   this.setState({ hidden: true });
+  // }
+
+  incrementIndex (length) {
+    this.timeout = setTimeout(() => {
+      let index = this.state.index;
+      if (index < length - 1) {
+        index += 1;
+        this.setState({ index });
+      }
+      else this.setState({ index: 0 });
+    }, 500);
   }
 
   render () {
@@ -36,25 +50,30 @@ export default class CartItem extends React.Component {
       !url.includes("cover") && !url.includes("gif")
     );
 
+    
     let tags = genresForGame(genres, gamesGenres, game.id).map((genre, index) =>
-      <div key={'genre' + index}>{genre}</div>
+    <div className='game-tag' key={'genre' + index}>{genre}</div>
     );
-
+    
     // hide game details if state is hidden
     let gameDetails;
     if (!this.state.hidden) {
+      this.incrementIndex(gamePictures.length);
       gameDetails = (
         <aside className="game-details">
+          <span className="material-icons">
+            arrow_left
+          </span>
           <h1>{game.title}</h1>
-          <h2>{game.releaseDate}</h2>
-          <img src={gamePictures[0]} />
-          <p className='game-field'>Genres:</p>
+          <h2>Released: {game.releaseDate}</h2>
+          <img src={gamePictures[this.state.index]} />
+          <h2 className='game-field'>Genres:</h2>
           <div className='game-tags'>
             {tags}
           </div>
         </aside>
       )
-    }
+    } else clearTimeout(this.timeout);
 
     return (
       <li 

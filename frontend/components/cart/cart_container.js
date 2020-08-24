5d76479e4fe9d787847777750c1ec1bb64fa853b
dancreppel <creppel@usc.edu>
2020-08-24 13:49:44 -0700

    Allow cart component to fetch genres and games genres

--- frontend/components/cart/cart_container.js
+++ frontend/components/cart/cart_container.js
@@ -4,6 +4,8 @@ import {
   fetchCartItems, 
   deleteCartItem 
 } from "../../actions/cart_actions";
+import { fetchGenres } from '../../actions/genres_actions';
+import { fetchGamesGenres } from '../../actions/games_genres_actions';
 
 const MSP = ({ entities: { cart, games } }) => ({
   cart,
@@ -12,7 +14,9 @@ const MSP = ({ entities: { cart, games } }) => ({
 
 const MDP = (dispatch) => ({
   getUserCart: () => dispatch(fetchCartItems()),
-  removeCartItem: (id) => dispatch(deleteCartItem(id))
+  removeCartItem: (id) => dispatch(deleteCartItem(id)),
+  fetchGamesGenres: () => dispatch(fetchGamesGenres()),
+  fetchGenres: () => dispatch(fetchGenres()),
 });
 
 export default connect(MSP, MDP)(Cart);

0698de2aadb1c35a6a63e5ee2ac3239636effd78
dancreppel <creppel@usc.edu>
2020-08-26 16:58:49 -0700

    Allow access to createLibraryItem action

--- frontend/components/games_show/purchase_game_container.js
+++ frontend/components/games_show/purchase_game_container.js
@@ -1,6 +1,7 @@
 import { connect } from 'react-redux'
 import PurchaseGame from './purchase_game';
 import { createCartItem } from "../../actions/cart_actions";
+import { createLibraryItem } from '../../actions/library_actions';
 
 const MSP = ({ sessionUser, entities: { cart } } ) => ({
   cart: Object.values(cart),
@@ -9,6 +10,7 @@ const MSP = ({ sessionUser, entities: { cart } } ) => ({
 
 const MDP = dispatch => ({
   addToCart: (gameId) => dispatch(createCartItem(gameId)),
+  addToWishlist: (gameId, owned) => dispatch(createLibraryItem(gameId, owned))
 });
 
 export default connect(MSP, MDP)(PurchaseGame);

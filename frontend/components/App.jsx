ff716d664f37db4c2d7e455cc4e452205d43e5a6
dancreppel <creppel@usc.edu>
2020-08-26 12:34:15 -0700

    Add protected routes for library and wishlist

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -6,6 +6,8 @@ import GamesIndexContainer from './games_index/games_index_container';
 import GameShowContainer from './games_show/games_show_container';
 import ProtectedRoute from './routes/protected_route';
 import CartContainer from './cart/cart_container';
+import LibraryContainer from './library-wishlist/library_container';
+import WishlistContainer from './library-wishlist/wishlist_container';
 
 const App = () => (
   <>
@@ -14,6 +16,8 @@ const App = () => (
     <Route exact path='/' component={GamesIndexContainer} />
     <Route path='/games/:id' component={GameShowContainer} />
     <ProtectedRoute path='/cart' component={CartContainer} />
+    <ProtectedRoute path='/library' component={LibraryContainer} />
+    <ProtectedRoute path='/wishlist' component={WishlistContainer} />
   </>
 );
 

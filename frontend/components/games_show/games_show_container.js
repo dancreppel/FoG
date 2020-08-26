b27938bb1dee655cb03640ec49d457874c3b5a70
dancreppel <creppel@usc.edu>
2020-08-26 16:57:29 -0700

    Allow access to library slice of state

--- frontend/components/games_show/games_show_container.js
+++ frontend/components/games_show/games_show_container.js
@@ -5,12 +5,14 @@ import { genresForGame } from '../../selectors/games_genres';
 import { fetchGenres } from '../../actions/genres_actions';
 import { fetchGamesGenres } from '../../actions/games_genres_actions';
 import { fetchCartItems } from '../../actions/cart_actions';
+import { fetchLibrary } from '../../actions/library_actions'
 
 
-const MSP = ({ entities: { games, genres, gamesGenres } }, ownProps ) => {
+const MSP = ({ entities: { games, genres, gamesGenres, library } }, ownProps ) => {
   return {
     game: games[ownProps.match.params.id],
-    genres: genresForGame(genres, gamesGenres, ownProps.match.params.id)
+    genres: genresForGame(genres, gamesGenres, ownProps.match.params.id),
+    library: Object.values(library)
   }
 }
 
@@ -18,6 +20,7 @@ const MDP = dispatch => ({
   fetchGame: gameId => dispatch(fetchGame(gameId)),
   fetchGenres: () => dispatch(fetchGenres()),
   fetchGamesGenres: () => dispatch(fetchGamesGenres()),
+  fetchLibrary: () => dispatch(fetchLibrary()),
   fetchCartItems: () => dispatch(fetchCartItems())
 });
 

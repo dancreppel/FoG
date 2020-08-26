97e01835085f15bc29452ad8272745de1eb3ee8b
dancreppel <creppel@usc.edu>
2020-08-25 19:28:10 -0700

    Add library reducer

--- frontend/reducers/entities/entities_reducer.js
+++ frontend/reducers/entities/entities_reducer.js
@@ -4,13 +4,15 @@ import gamesReducer from './games_reducer';
 import gamesGenresReducer from './games_genres_reducer';
 import genresReducer from './genres_reducer';
 import cartReducer from './cart_reducer';
+import libraryReducer from "./library_reducer";
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   games: gamesReducer,
   gamesGenres: gamesGenresReducer,
   genres: genresReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  library: libraryReducer
 });
 
 export default entitiesReducer;

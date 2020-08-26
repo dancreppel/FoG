490c3a6234dd98821e1c50d9a81cdb4235cf4278
dancreppel <creppel@usc.edu>
2020-08-25 18:17:15 -0700

    Make create action for libraries controller and fix index action typos

--- app/controllers/api/libraries_controller.rb
+++ app/controllers/api/libraries_controller.rb
@@ -3,12 +3,32 @@ class Api::LibrariesController < ApplicationController
 
   def index
     @library_items = current_user.library_items
-    @library_games = current_user.library_items.with_attached_photos
+    @library_games = current_user.library_games.with_attached_photos
 
-    if @libary_items && @library_games
-      render json: [@libary_items, @library_games]
+    debugger;
+    if @library_items && @library_games
+      render json: [@library_items, @library_games]
     else
       render json: ["Library is empty"], status: 404
     end
+  end
+
+  def create 
+    @library_item = Library.new(
+      user_id: current_user.id,
+      game_id: library_params[:game_id],
+      owned: library_params[:owned]
+    )
+
+    if @library_item.save
+      render json: @library_item
+    else
+      render json: @library_item.errors.full_messages, status: 422
+    end
+  end
+
+  private
+  def library_params 
+    params.require(:library).permit(:game_id, :owned)
   end
 end

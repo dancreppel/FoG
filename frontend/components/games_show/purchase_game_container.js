e9dd70cfae113bca8ac84e3a1b7924574848fbac
dancreppel <creppel@usc.edu>
2020-08-25 17:14:46 -0700

    Allow access to sessionUser

--- frontend/components/games_show/purchase_game_container.js
+++ frontend/components/games_show/purchase_game_container.js
@@ -2,8 +2,9 @@ import { connect } from 'react-redux'
 import PurchaseGame from './purchase_game';
 import { createCartItem } from "../../actions/cart_actions";
 
-const MSP = ({ entities: { cart } } ) => ({
-  cart: Object.values(cart)
+const MSP = ({ sessionUser, entities: { cart } } ) => ({
+  cart: Object.values(cart),
+  sessionUser
 });
 
 const MDP = dispatch => ({

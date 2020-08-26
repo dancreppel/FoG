dc54ecb87740dcac61330dba0b6286105b10986d
dancreppel <creppel@usc.edu>
2020-08-25 19:27:50 -0700

    Remove functions tested on window

--- frontend/FoG.jsx
+++ frontend/FoG.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root';
 import configureStore from './store/store';
-import * as CartActions from "./actions/cart_actions"
 
 document.addEventListener( 'DOMContentLoaded', () => {
   const root = document.getElementById('root');
@@ -11,12 +10,6 @@ document.addEventListener( 'DOMContentLoaded', () => {
   if (window.currentUser) preloadedState = { sessionUser: currentUser }
   const store = configureStore(preloadedState);
   delete window.currentUser;
-
-  // TODO TEST CART REDUCER WITH THIS
-  window.dispatch = store.dispatch;
-  window.fetchCartItems = CartActions.fetchCartItems;
-  window.createCartItem = CartActions.createCartItem;
-  window.deleteCartItem = CartActions.deleteCartItem;
 
   ReactDOM.render(<Root store={store}/>, root)
 });

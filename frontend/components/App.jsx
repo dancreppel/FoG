74e195810eafca5220d210e5244f0662259f464f
dancreppel <creppel@usc.edu>
2020-08-26 17:48:52 -0700

    Use library wishlist container

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -7,7 +7,7 @@ import GameShowContainer from './games_show/games_show_container';
 import ProtectedRoute from './routes/protected_route';
 import CartContainer from './cart/cart_container';
 import LibraryContainer from './library-wishlist/library_container';
-import WishlistContainer from './library-wishlist/wishlist_container';
+import LibraryWishlistContainer from './library-wishlist/library_wishlist_container';
 
 const App = () => (
   <>
@@ -16,8 +16,8 @@ const App = () => (
     <Route exact path='/' component={GamesIndexContainer} />
     <Route path='/games/:id' component={GameShowContainer} />
     <ProtectedRoute path='/cart' component={CartContainer} />
-    <ProtectedRoute path='/library' component={LibraryContainer} />
-    <ProtectedRoute path='/wishlist' component={WishlistContainer} />
+    <ProtectedRoute path='/library' component={LibraryWishlistContainer} />
+    <ProtectedRoute path='/wishlist' component={LibraryWishlistContainer} />
   </>
 );
 

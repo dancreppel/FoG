ef12d3ece5185ad17c66b5cbf014336188c4cfa4
dancreppel <creppel@usc.edu>
2020-08-26 18:32:08 -0700

    Map library slice of state to props

--- frontend/components/games_show/purchase_game_container.js
+++ frontend/components/games_show/purchase_game_container.js
@@ -3,8 +3,9 @@ import PurchaseGame from './purchase_game';
 import { createCartItem } from "../../actions/cart_actions";
 import { createLibraryItem } from '../../actions/library_actions';
 
-const MSP = ({ sessionUser, entities: { cart } } ) => ({
+const MSP = ({ sessionUser, entities: { cart, library } } ) => ({
   cart: Object.values(cart),
+  library: Object.values(library),
   sessionUser
 });
 
